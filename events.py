326e5cd0af1d1a50a8a6cb6f42ac315ce9bd3aac
Felipe Orellana <forellana@digithinkit.com>
2016-07-15 14:59:05 +0000

    Added event example usage

--- events.py
+++ events.py
@@ -7,8 +7,14 @@ class Event(object):
         def my_handler():
             print("Handled")
 
+        def my_other_handler():
+            print("Other Handler")
+
         my_signal = Event()
         my_signal += my_handler
+
+        #also valid:
+        my_signal += [my_handler, my_other_handler]
 
         my_signal()
     
@@ -23,12 +29,19 @@ class Event(object):
         """Adds a callback handler"""
 
         if handler is not None:
-            self._signals.append(handler)
+            if isinstance(handler, list):
+                self._signals.extend(handler)
+            else:
+                self._signals.append(handler)
 
     def remove(self, handler):
         """Removes a callback handler"""
-        if handler in self._signals:
-            self._signals.remove(handler)
+        if not isinstance(handler, list):
+            handler = [handler]
+
+        for h in handler:
+            if h in self._signals:
+                self._signals.remove(h)
 
     def __call__(self, *args, **kwargs):
         """Syntax sugar, allows calling instance as a method"""
@@ -54,3 +67,42 @@ class Event(object):
         self.remove(other)
         return self
 
+if __name__ == "__main__":
+    # example usage
+    def handler1(e):
+        print(" - Handler1: %s" % e)
+
+    def handler2(e):
+        print(" - Handler2: %s" % e)
+
+
+    # create signal/slot
+    signal = Event()
+    
+    # add event handler
+    signal += handler1
+
+    # call event
+    print("- First Call")
+    signal("First Call")
+
+    # add second handler
+    signal += handler2
+
+    # call event
+    print("\n- Second Call")
+    signal("Second Call")
+
+    # remove all handlers
+    signal -= handler1
+    signal -= handler2
+
+    #event call
+    print("\n- Third Call(no handlers)")
+
+    # add multiple handlers at once
+    signal += [handler1, handler2]
+
+    #event call
+    print("\n- Fourth Call")
+    signal("Fourth Call")
